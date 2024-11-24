6fa0d647909fdb78e9f85dbb574602b40326bcc4
dloza18 <146028015+dloza18@users.noreply.github.com>
2024-11-23 21:55:26 -0600

    Add files via upload

---
+++ CatalogoDBcontext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Catalogo.Modelos
+{
+    public class CatalogoDBContext : DbContext
+    {
+        public CatalogoDBContext(DbContextOptions<CatalogoDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Persona> Personas { get; set; }
+        public DbSet<Clasificacion> Clasificaciones { get; set; }
+        public DbSet<Habito>Habitos { get; set; }
+    }
+}
---
+++ Persona.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalogo.Modelos
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [StringLength(100, ErrorMessage = "Máximo 100 caracteres")]
+        public string? Nombre { get; set; }
+        [Required(ErrorMessage = "El correo es requerido")]
+        [EmailAddress(ErrorMessage = "Debe ser un correo válido")]
+        [StringLength(100, ErrorMessage = "Máximo 100 caracteres")]
+        public string? Correo { get; set; }
+        [Required(ErrorMessage = "El teléfono es requerido")]
+        [StringLength(10, ErrorMessage = "Máximo 10 caracteres")]
+        public string? Telefono { get; set; }
+
+        //Propiedades de navegación EF
+        public int ClasificacionId { get; set; }
+        virtual public Clasificacion? Clasificacion { get; set; }
+        virtual public ICollection<Habito>?Habitos { get; set; }
+    }
+}
